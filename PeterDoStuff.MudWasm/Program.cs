041121a333e67ec1b47e1a1285492a2617839313
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-01-13 16:37:57 +0800

    Add reference of the Game class, create the basic UI

--- PeterDoStuff.MudWasm/Program.cs
+++ PeterDoStuff.MudWasm/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using PeterDoStuff;
 using PeterDoStuff.MudWasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+builder.Services.AddScoped(_ => new GameOfLife(1,1));
 
 await builder.Build().RunAsync();
