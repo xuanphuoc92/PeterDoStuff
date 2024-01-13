062b5cedd5d7018c217c207a4fadab408564460f
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-01-13 20:51:35 +0800

    Improve the tests

--- PeterDoStuff.Test/GameOfLife_Test.cs
+++ PeterDoStuff.Test/GameOfLife_Test.cs
@@ -9,7 +9,7 @@ namespace PeterDoStuff.Test
         [TestMethod]
         public void _01_DeadDueToIsolation()
         {
-            // [X]
+            // [O]
             var game = new GameOfLife(1, 1);
             game.GetCell(1, 1).State = CellState.Live;
             game.Next();
@@ -19,35 +19,56 @@ namespace PeterDoStuff.Test
         [TestMethod]
         public void _02_StayLive()
         {
-            // [ ][X][ ]
-            // [X][ ][X]
-            // [ ][X][ ]
+            // [ ][O][ ]
+            // [O][ ][O]
+            // [ ][O][ ]
             var game = new GameOfLife(3, 3);
             game.GetCell(1, 2).State = CellState.Live;
             game.GetCell(2, 1).State = CellState.Live;
             game.GetCell(3, 2).State = CellState.Live;
             game.GetCell(2, 3).State = CellState.Live;
             game.Next();            
-            game.GetCell(1, 2).State.Should().Be(CellState.Live);
-            game.GetCell(2, 1).State.Should().Be(CellState.Live);
-            game.GetCell(3, 2).State.Should().Be(CellState.Live);
-            game.GetCell(2, 3).State.Should().Be(CellState.Live);
+
+            for (int x = 1; x <= 3; x++)
+                for (int y = 1; y <= 3; y++)
+                {
+                    if (x + y == 3 || x + y == 5)
+                        game.GetCell(x, y).State.Should().Be(CellState.Live);
+                    else
+                        game.GetCell(x, y).State.Should().Be(CellState.Dead);
+                }
         }
 
         [TestMethod]
-        public void _03_DeadDueToOverpopulation()
+        public void _03_CenterDeadDueToOverpopulation_CornerResurrect()
         {
-            // [ ][X][ ]
-            // [X][X][X]
-            // [ ][X][ ]
+            // From:
+            // [ ][O][ ]
+            // [O][O][O]
+            // [ ][O][ ]
+
+            // To:
+            // [O][O][O]
+            // [O][ ][O]
+            // [O][O][O]
+
             var game = new GameOfLife(3, 3);
             game.GetCell(1, 2).State = CellState.Live;
             game.GetCell(2, 1).State = CellState.Live;
             game.GetCell(3, 2).State = CellState.Live;
             game.GetCell(2, 3).State = CellState.Live;
             game.GetCell(2, 2).State = CellState.Live;
+            
             game.Next();
-            game.GetCell(2, 2).State.Should().Be(CellState.Dead);
+
+            for (int x = 1; x <= 3; x++)
+                for (int y = 1; y <= 3; y++)
+                {
+                    if (x == 2 && y == 2)
+                        game.GetCell(x, y).State.Should().Be(CellState.Dead);
+                    else
+                        game.GetCell(x, y).State.Should().Be(CellState.Live);
+                }
         }
     }
 }
