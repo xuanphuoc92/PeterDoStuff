29cbe919994a75fc1f822f5356fec2db4966c403
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-01-22 23:52:05 +0800

    dev: Add and update the 3 games into the client web assembly

--- PeterDoStuff.MudWasmHosted/Client/Program.cs
+++ PeterDoStuff.MudWasmHosted/Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using PeterDoStuff;
 using PeterDoStuff.MudWasmHosted.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,5 +10,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+builder.Services.AddScoped(_ => new GameOfLife(1, 1));
+builder.Services.AddScoped(_ => MineSweeper.New(10, 10).RandomizeMines(10).Start());
+builder.Services.AddScoped(_ => MatchFinder.New().Reset().AddRandoms(30));
 
 await builder.Build().RunAsync();
