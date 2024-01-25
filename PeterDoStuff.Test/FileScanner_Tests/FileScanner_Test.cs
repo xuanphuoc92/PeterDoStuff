e160356e14e944cf51e342ecc6e0586ddc07353e
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-01-25 23:40:21 +0800

    file-scanner: Implement and test FileScanner

---
+++ PeterDoStuff.Test/FileScanner_Tests/FileScanner_Test.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Test.FileScanner_Tests
+{
+    [TestClass]
+    public class FileScanner_Test
+    {
+        [TestMethod]
+        public void _01_ScanZipFile()
+        {
+            using var scanner = new FileScanner();
+            var result = scanner.ScanZip("FileScanner_Tests\\TestZipFile.zip");
+            result.Should().Be("Zip scan is successful");
+            scanner.ZipStats.Count.Should().Be(4);
+            string csvContent = scanner.ToCsv();
+            Console.WriteLine(csvContent);
+        }
+
+        [TestMethod]
+        public void _02_ScanNonExistFile()
+        {
+            using var scanner = new FileScanner();
+            var result = scanner.ScanZip("NonExistFile.zip");
+            result.Should().NotBe("Zip scan is successful");
+            Console.WriteLine(result);
+        }
+
+        [TestMethod]
+        public void _03_ScanNonZipFile()
+        {
+            using var scanner = new FileScanner();
+            var result = scanner.ScanZip("FileScanner_Tests\\TestTextFile.txt");
+            result.Should().NotBe("Zip scan is successful");
+            Console.WriteLine(result);
+        }
+    }
+}
