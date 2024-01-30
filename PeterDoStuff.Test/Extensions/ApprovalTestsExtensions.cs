80a127aacd652b3cad7e2224d0442f8832d6b9ca
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-01-30 13:13:20 +0800

    approval-test: Organize the Extensions. Apply Approval for 1 test case.

--- PeterDoStuff.Test/Extensions/ApprovalTestsExtensions.cs
+++ PeterDoStuff.Test/Extensions/ApprovalTestsExtensions.cs
@@ -4,6 +4,10 @@ namespace PeterDoStuff.Test.Extensions
 {
     public static class ApprovalTestsExtensions
     {
+        /// <summary>
+        /// Verify via Approval Tests
+        /// </summary>
+        /// <param name="this"></param>
         public static void Verify(this string @this)
             => Approvals.Verify(@this);
     }
