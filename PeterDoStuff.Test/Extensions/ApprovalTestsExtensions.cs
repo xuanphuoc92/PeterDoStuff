52f754b832a1572785be0e23540d712cab93fe25
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-01-30 12:39:36 +0800

    approval-test: Integrate Approval Tests for GameOfLife

---
+++ PeterDoStuff.Test/Extensions/ApprovalTestsExtensions.cs
@@ -0,0 +1,10 @@
+﻿using ApprovalTests;
+
+namespace PeterDoStuff.Test.Extensions
+{
+    public static class ApprovalTestsExtensions
+    {
+        public static void Verify(this string @this)
+            => Approvals.Verify(@this);
+    }
+}
