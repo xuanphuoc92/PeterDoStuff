c429baf6de8ff997f6ed0026499dac28ba0265e0
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-02-21 19:43:35 +0800

    environment: Setup the API to read the Environment Variable values

---
+++ PeterDoStuff.MudWasmHosted/Server/Controllers/EnvironmentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeterDoStuff.MudWasmHosted.Shared;
+
+namespace PeterDoStuff.MudWasmHosted.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EnvironmentController : ControllerBase
+    {
+        private const string empty = "Such an empty environment... Why not try setting an Environment Variable named MyFirstEnvironment?";
+
+        [HttpGet]
+        public string Get()
+        {
+            var environment = Environment.GetEnvironmentVariable("MyFirstEnvironment");
+            return environment == null 
+                ? empty 
+                : $"Huray! MyFirstEnvironment is set as {environment}";
+        }
+    }
+}
