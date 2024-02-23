aeda91ecf2f92477ac844f3d42cbad10e01bf1b2
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-02-23 14:39:51 +0800

    database: Setup the database access controller (not used yet)

--- PeterDoStuff.MudWasmHosted/Server/Controllers/DatabaseController.cs
+++ PeterDoStuff.MudWasmHosted/Server/Controllers/DatabaseController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PeterDoStuff.Database;
+using PeterDoStuff.MudWasmHosted.Shared;
 
 namespace PeterDoStuff.MudWasmHosted.Server.Controllers
 {
@@ -24,6 +25,27 @@ namespace PeterDoStuff.MudWasmHosted.Server.Controllers
         public Task<DbOutput> Post([FromBody] string sql)
         {
             return _db.ExecuteOrQueryAsync(sql);
+        }
+
+        private const string DEFAULT_ACCESS_KEY = "access";
+
+        [HttpPost]
+        [Route("access")]
+        public DbAccess Access([FromBody] string accessKey)
+        {
+            var dbAccess = new DbAccess();
+            
+            var databaseAccessKey = Environment.GetEnvironmentVariable("DatabaseAccessKey");
+            if (databaseAccessKey == null)
+            {
+                dbAccess.Warning = "You are using the default access key. Please set Environment Variable [DatabaseAccessKey] to stop using the default access key.";
+                databaseAccessKey = DEFAULT_ACCESS_KEY;
+            }
+
+            dbAccess.IsSuccess = accessKey == databaseAccessKey;
+            dbAccess.Token = dbAccess.IsSuccess ? databaseAccessKey : "";
+
+            return dbAccess;
         }
     }
 }
---
+++ PeterDoStuff.MudWasmHosted/Shared/DbAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.MudWasmHosted.Shared
+{
+    public class DbAccess
+    {
+        public bool IsSuccess { get; set; }
+        public string Warning { get; set; }
+        public string Token { get; set; }
+    }
+}
