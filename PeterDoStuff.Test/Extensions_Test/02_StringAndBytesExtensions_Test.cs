8ff2045b60f3ae86dd87addccdf3a29ec3de6f68
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-03-02 19:52:37 +0800

    security: Implement string extensions, first is the MD5 Hash

---
+++ PeterDoStuff.Test/Extensions_Test/02_StringAndBytesExtensions_Test.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using PeterDoStuff.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Test.Extensions_Test
+{
+    [TestClass]
+    public class _02_StringAndBytesExtensions_Test
+    {
+        [TestMethod]
+        public void _01_MD5Hash()
+        {
+            "Hello"
+                .ToByteArray()
+                .ToMD5Hash()
+                .ToHexString()
+                .Should().Be("8b1a9953c4611296a827abf8c47804d7");
+        }
+    }
+}
---
+++ PeterDoStuff/Extensions/StringAndBytesExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Extensions
+{
+    public static class StringAndBytesExtensions
+    {
+        public static byte[] ToByteArray(this string input) 
+            => Encoding.UTF8.GetBytes(input);
+
+        public static byte[] ToMD5Hash(this byte[] input)
+        {
+            using MD5 md5 = MD5.Create();
+            return md5.ComputeHash(input);
+        }
+
+        public static string ToHexString(this byte[] input) 
+            => input
+                .Select(x => x.ToString("x2"))
+                .Join();
+
+        public static string Join(this IEnumerable<string> input, string separator = "")
+            => string.Join(separator, input);
+    }
+}
