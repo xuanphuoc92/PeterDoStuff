7bb3a3e96ea2cc586fccdd5cd78568364fc08b95
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-03-04 15:06:21 +0800

    security: Implement the server side hashing for Argon2id

--- PeterDoStuff.MudWasmHosted/Server/Controllers/SecurityController.cs
+++ PeterDoStuff.MudWasmHosted/Server/Controllers/SecurityController.cs
@@ -8,6 +8,28 @@ namespace PeterDoStuff.MudWasmHosted.Server.Controllers
     public class SecurityController : ControllerBase
     {
         [HttpGet]
+        [Route("HashArgon2id")]
+        public byte[] HashArgon2id(string input, string salt)
+        {
+            return input
+                .ToByteArray()
+                .HashArgon2id(salt.ToByteArrayAsHexString());
+        }
+
+        [HttpGet]
+        [Route("HashArgon2idQuick")]
+        public byte[] HashArgon2idQuick(string input, string salt)
+        {
+            return input
+                .ToByteArray()
+                .HashArgon2id(salt.ToByteArrayAsHexString(),
+                    iterations: 1,
+                    memorySize: 10,
+                    degreeOfParallelism: 1
+                );
+        }
+
+        [HttpGet]
         [Route("EncryptAes")]
         public byte[] EncryptAes(string input, string key, string iv)
         {
