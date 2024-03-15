54ccbefad9ad1c99867fefbe6238ba8c70b73147
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-03-15 13:09:35 +0800

    2048: Setup the game

---
+++ PeterDoStuff.Test/2048_Test.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using PeterDoStuff._2048;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Test
+{
+    [TestClass]
+    public class _2048_Test
+    {
+        [TestMethod]
+        public void _01_StartBlocks()
+        {
+            var game = new Game(0, 2);
+            game.Blocks.Should().HaveCount(2);
+            
+            game.Blocks[0].Number.Should().Be(2);
+            game.Blocks[0].LocationIndex.Should().Be(0);
+            game.Blocks[0].X.Should().Be(0);
+            game.Blocks[0].Y.Should().Be(0);
+
+            game.Blocks[1].Number.Should().Be(2);
+            game.Blocks[1].LocationIndex.Should().Be(2);
+            game.Blocks[1].X.Should().Be(2);
+            game.Blocks[1].Y.Should().Be(0);
+
+            game = new Game();
+            game.Blocks.Should().HaveCount(Game.START_BLOCKS);            
+        }
+    }
+}
---
+++ PeterDoStuff/2048/Game.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff._2048
+{
+    public class Game
+    {
+        public List<Block> Blocks { get; private set; }
+        
+        public int Width { get; private set; } = 4;
+        public int Height { get; private set; } = 4;
+        
+        public const int START_BLOCKS = 2;
+
+        public Game()
+        {
+            var allBlocks = Enumerable.Range(0, Width * Height);
+            var random = new Random();
+            allBlocks.OrderBy(b => random.Next());
+            var startBlockLocations = allBlocks
+                .OrderBy(b => random.Next()) // Shuffle
+                .Take(START_BLOCKS); // Take the first 2
+
+            SetupStartBlocks(startBlockLocations);
+        }
+
+        public Game(params int[] startBlockLocations)
+        {
+            SetupStartBlocks(startBlockLocations);
+        }
+
+        private void SetupStartBlocks(IEnumerable<int> startBlockLocations)
+        {
+            Blocks = new List<Block>();
+            foreach (var location in startBlockLocations)
+                Blocks.Add(new Block(location, this));
+        }
+    }
+
+    public class Block
+    {
+        public int Number { get; private set; }
+        public int LocationIndex { get; private set; }
+        public Game Game { get; private set; }
+
+        public Block(int locationIndex, Game game)
+        {
+            Number = 2; // Every block created must be 2
+            LocationIndex = locationIndex;
+            Game = game;
+        }
+
+        public int X
+        {
+            get => LocationIndex % Game.Width;
+        }
+
+        public int Y
+        {
+            get => LocationIndex / Game.Height;
+        }
+    }
+}
