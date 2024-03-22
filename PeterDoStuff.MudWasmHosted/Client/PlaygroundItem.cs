5b2429b0f228c6de7c119a52c0f7b837da548626
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-03-23 01:13:06 +0800

    smart-component: Refine and update the UI

--- PeterDoStuff.MudWasmHosted/Client/PlaygroundItem.cs
+++ PeterDoStuff.MudWasmHosted/Client/PlaygroundItem.cs
@@ -19,7 +19,7 @@ namespace PeterDoStuff.MudWasmHosted.Client
 
         public static readonly List<PlaygroundItem> Items = new List<PlaygroundItem>()
         {
-            new("smartComponent", Icons.Material.Filled.Assistant, "Smart Components", "This page demonstrates of smart components from Microsoft."),
+            new("smartComponent", Icons.Material.Filled.Lightbulb, "Smart Components", "This page demonstrates of smart components."),
             new("qrCode", Icons.Material.Filled.QrCode, "QR Code", "This page demonstrates the QR code generating and scanning tools."),
             new("chaos2048", Icons.Material.Outlined._2k, "Chaos 2048", "This page demonstrates the game 2048 (with chaos animation mode)"),
             new("snake", Icons.Material.Outlined.Earbuds, "Snake", "This page demonstrates the game Snake."),
