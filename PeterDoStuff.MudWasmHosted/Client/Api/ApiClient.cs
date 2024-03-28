3826599898b3a4915e95422006eff642aa0d1540
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-03-28 14:01:16 +0800

    api-meta: Refine with the exception throwing and handling

--- PeterDoStuff.MudWasmHosted/Client/Api/ApiClient.cs
+++ PeterDoStuff.MudWasmHosted/Client/Api/ApiClient.cs
@@ -1,4 +1,5 @@
 ﻿using PeterDoStuff.MudWasmHosted.Client.Extensions;
+using System.Reflection.Metadata.Ecma335;
 
 namespace PeterDoStuff.MudWasmHosted.Client.Api
 {
@@ -20,7 +21,14 @@ namespace PeterDoStuff.MudWasmHosted.Client.Api
             var result = await Http.Request(method, url)
                 .SetBody(body)
                 .SendAsync<TReturn>();
-            return result.Result;
+            if (result.Success)
+                return result.Result;
+            throw new ApiException($"Api Error: {result.Error}");
         }
+    }
+
+    public class ApiException : Exception
+    {
+        public ApiException(string message) : base(message) { }
     }
 }
