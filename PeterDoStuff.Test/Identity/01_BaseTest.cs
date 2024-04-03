312083b3340f7a3c2cd59ca6bf3691665004a3ec
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-04-03 17:50:38 +0800

    identity: Implement the user model and first service, Register

---
+++ PeterDoStuff.Test/Identity/01_BaseTest.cs
@@ -0,0 +1,116 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using PeterDoStuff.Identity;
+using PeterDoStuff.Test.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Test.Identity
+{
+    [TestClass]
+    public class _01_BaseTest
+    {
+        private UserContext GetTestContext()
+        {
+            var options = new DbContextOptionsBuilder<UserContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            return new UserContext(options);
+        }
+
+        [TestMethod]
+        public void _01_Setup()
+        {
+            using (var context = GetTestContext())
+            {
+                context.Database.EnsureCreated().Should().BeTrue();
+                context.Model.FindEntityType(typeof(User)).Should().NotBeNull();
+                context.Model.FindEntityType(typeof(UserAuth)).Should().NotBeNull();
+            }
+        }
+
+        [TestMethod]
+        public void _02_Add()
+        {
+            using (var context = GetTestContext())
+            {
+                context.Users.Add(new User() { Name = "Test" });
+                context.UserAuths.Add(new UserAuth());
+                context.SaveChanges();
+            }
+
+            using (var context = GetTestContext())
+            {
+                context.Users.Count().Should().Be(1);
+                context.UserAuths.Count().Should().Be(1);
+                
+                var user = context.Users.Single();
+                user.Id.ToString().WriteToConsole();
+                user.Id.Should().NotBe(new Guid());
+            }
+        }
+
+        [TestMethod]
+        public void _03_AddRelation()
+        {
+            Guid userId, userAuthId;
+
+            using (var context = GetTestContext())
+            {   
+                User user = new User() { Name = "Test" };
+                user.Auths = [new UserAuth()];
+                context.Users.Add(user);
+                context.SaveChanges();
+
+                userId = user.Id;
+                userAuthId = user.Auths.Single().Id;
+            }
+
+            userId.ToString().WriteToConsole("userId");
+            userAuthId.ToString().WriteToConsole("userAuthId");
+
+            using (var context = GetTestContext())
+            {
+                User? user = context.Users.Find(userId);
+                
+                user.Should().NotBeNull();
+                user.Id.Should().Be(userId);
+                user.Auths.Should().BeNullOrEmpty();
+
+                user = context.Users
+                    .Include(u => u.Auths)
+                    .AsNoTracking()
+                    .SingleOrDefault(u => u.Id == userId);
+
+                user.Should().NotBeNull();
+                user.Id.Should().Be(userId);
+                user.Auths.Should().NotBeNullOrEmpty();
+                user.Auths.Single().Id.Should().Be(userAuthId);
+                user.Auths.Single().UserId.Should().Be(userId);
+
+            }
+        }
+
+        [TestMethod]
+        public void _04_Register()
+        {
+            using (var context = GetTestContext())
+            {
+                var service = new UserService(context);
+                var result = service.Register("NewUser", "P@ssw0rd");
+                result.Should().BeTrue();
+            }
+
+            using (var context = GetTestContext())
+            {
+                var service = new UserService(context);
+                var result = service.Register("NewUser", "P@ssw0rd");
+                result.Should().BeFalse();
+            }
+        }
+    }
+}
---
+++ PeterDoStuff/Identity/User.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeterDoStuff.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Identity
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public ICollection<UserAuth>? Auths { get; set; }
+    }
+
+    public class UserAuth
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+
+        [MaxLength(100)]
+        public string? UserName { get; set; }
+        [MaxLength(32)]
+        public byte[]? PasswordHash { get; set; }
+        [MaxLength(16)]
+        public byte[]? PasswordSalt { get; set; }
+    }
+
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<UserAuth> UserAuths => Set<UserAuth>();
+    }
+
+    public class UserService
+    {
+        private readonly UserContext _context;
+        public UserService (UserContext context)
+        {
+            _context = context;
+        }
+
+        public bool Register(string userName, string password)
+        {
+            userName = userName.ToLower();
+            var auth = _context.UserAuths
+                .SingleOrDefault(ua => ua.UserName != null && ua.UserName.ToLower() == userName);
+            if (auth != null)
+                return false;
+
+            var user = new User();
+            user.Name = userName;
+            user.Auths = new List<UserAuth>();
+            
+            auth = new UserAuth();
+            auth.UserName = userName;
+            auth.PasswordSalt = SecurityExtensions.GenerateSalt();
+            auth.PasswordHash = password.ToByteArray().HashArgon2id(auth.PasswordSalt);
+
+            user.Auths.Add(auth);
+
+            _context.Users.Add(user);
+
+            _context.SaveChanges();
+
+            return true;
+        }
+    }
+}
