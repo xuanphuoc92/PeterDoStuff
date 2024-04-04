0722e2f7c51abdf627db48667cf4183c86604975
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-04-04 23:48:11 +0800

    migration: implement and test the first draft of migration

---
+++ PeterDoStuff.Test/Extensions_Test/06_EfCoreExtensions_Test.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeterDoStuff.Extensions;
+using PeterDoStuff.Identity;
+using PeterDoStuff.Test.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Test.Extensions_Test
+{
+    [TestClass]
+    public class _06_EfCoreExtensions_Test
+    {
+        private UserContext GetTestContext()
+        {
+            var options = new DbContextOptionsBuilder<UserContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            return new UserContext(options);
+        }
+
+        [TestMethod]
+        public void _01_GetMigrateSql()
+        {
+            using var context = GetTestContext();
+            context.GetMigrateSql().WriteToConsole();
+        }
+    }
+}
