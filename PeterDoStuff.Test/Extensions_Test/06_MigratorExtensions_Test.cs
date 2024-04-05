b034bf3778e23ea2ebce09a9383f0867346d03e1
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-04-06 00:47:17 +0800

    validator: Make an own test suite for validator extensions

--- PeterDoStuff.Test/Extensions_Test/06_MigratorExtensions_Test.cs
+++ PeterDoStuff.Test/Extensions_Test/06_MigratorExtensions_Test.cs
@@ -78,52 +78,5 @@ namespace PeterDoStuff.Test.Extensions_Test
             using var context = GetTestContext();
             context.GetMigrator().Sql().Verify();
         }
-
-        [TestMethod]
-        public void _02_IsValid()
-        {
-            var entity = new TestEntity();
-            entity.IsValid().Should().BeTrue();
-
-            entity.Name = new string('t', 101);
-            entity.IsValid(out var validationResults).Should().BeFalse();
-            validationResults.Should().HaveCount(1);
-            validationResults[0].ErrorMessage.WriteToConsole("Exceed 100 string");
-
-            entity.Name = new string('t', 100);
-            entity.IsValid().Should().BeTrue();
-
-            entity.StandardHash = SecurityExtensions.GenerateSalt(33);
-            entity.IsValid(out validationResults).Should().BeFalse();
-            validationResults.Should().HaveCount(1);
-            validationResults[0].ErrorMessage.WriteToConsole("Exceed 32 hash");
-
-            entity.StandardHash = SecurityExtensions.GenerateSalt(32);
-            entity.IsValid().Should().BeTrue();
-
-            entity.CustomDecimal = 12.345678901m;
-            entity.IsValid(out validationResults).Should().BeFalse();
-            validationResults.Should().HaveCount(1);
-            validationResults[0].ErrorMessage.WriteToConsole("Exceed scale");
-
-            entity.CustomDecimal = 123456789012345678901m;
-            entity.IsValid(out validationResults).Should().BeFalse();
-            validationResults.Should().HaveCount(1);
-            validationResults[0].ErrorMessage.WriteToConsole("Exceed precision");
-
-            entity.CustomDecimal = 123456789001.23456789m;
-            entity.IsValid().Should().BeTrue();
-
-            entity.CustomDecimal = 123456789012;
-            entity.IsValid().Should().BeTrue();
-
-            entity.CreatedDate = DateTime.Today.AddMinutes(1);
-            entity.IsValid(out validationResults).Should().BeFalse();
-            validationResults.Should().HaveCount(1);
-            validationResults[0].ErrorMessage.WriteToConsole("Not Date only");
-
-            entity.CreatedDate = DateTime.Today;
-            entity.IsValid().Should().BeTrue();
-        }
     }
 }
