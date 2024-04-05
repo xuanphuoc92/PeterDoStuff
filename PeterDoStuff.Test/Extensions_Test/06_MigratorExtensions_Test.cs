9794e49535962d3bd5c6916689b66d037988ff29
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-04-05 22:38:21 +0800

    validator: Implement validator

--- PeterDoStuff.Test/Extensions_Test/06_MigratorExtensions_Test.cs
+++ PeterDoStuff.Test/Extensions_Test/06_MigratorExtensions_Test.cs
@@ -1,4 +1,5 @@
 ﻿using ApprovalTests.Reporters;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using PeterDoStuff.Extensions;
 using PeterDoStuff.Identity;
@@ -75,6 +76,42 @@ namespace PeterDoStuff.Test.Extensions_Test
         {
             using var context = GetTestContext();
             context.GetMigrator().Sql().Verify();
+        }
+
+        [TestMethod]
+        public void _02_IsValid()
+        {
+            var entity = new TestEntity();
+            entity.IsValid().Should().BeTrue();
+
+            entity.Name = new string('t', 101);
+            entity.IsValid(out var validationResults).Should().BeFalse();
+            validationResults.Should().HaveCount(1);
+            validationResults[0].ErrorMessage.WriteToConsole("Exceed 100 string");
+
+            entity.Name = new string('t', 100);
+            entity.IsValid().Should().BeTrue();
+
+            entity.StandardHash = SecurityExtensions.GenerateSalt(33);
+            entity.IsValid(out validationResults).Should().BeFalse();
+            validationResults.Should().HaveCount(1);
+            validationResults[0].ErrorMessage.WriteToConsole("Exceed 32 hash");
+
+            entity.StandardHash = SecurityExtensions.GenerateSalt(32);
+            entity.IsValid().Should().BeTrue();
+
+            entity.CustomDecimal = 12.345678901m;
+            entity.IsValid(out validationResults).Should().BeFalse();
+            validationResults.Should().HaveCount(1);
+            validationResults[0].ErrorMessage.WriteToConsole("Exceed scale");
+
+            entity.CustomDecimal = 123456789012345678901m;
+            entity.IsValid(out validationResults).Should().BeFalse();
+            validationResults.Should().HaveCount(1);
+            validationResults[0].ErrorMessage.WriteToConsole("Exceed precision");
+
+            entity.CustomDecimal = 123456789001.23456789m;
+            entity.IsValid().Should().BeTrue();
         }
     }
 }
