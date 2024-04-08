c8243677fd899816e217faefae7cc30b395403a4
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-04-08 12:51:38 +0800

    deletable-dbset: Add the base implementation

---
+++ PeterDoStuff.Test/Database/_03_DeletableDbSet_Test.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeterDoStuff.Database.EF;
+using PeterDoStuff.Extensions;
+using PeterDoStuff.Test.Extensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeterDoStuff.Test.Database
+{
+    [TestClass]
+    public class _03_DeletableDbSet_Test
+    {
+        private class DeletableEntity
+        {
+            public int Id { get; set; }
+            
+            [MaxLength(100)]
+            public string Name { get; set; }
+        }
+
+        private class DeletableContext : DbContext
+        {
+            public DeletableContext(DbContextOptions<DeletableContext> options) : base(options) { }
+
+            public DeletableDbSet<DeletableEntity> __DeletableTestTable__ { get; set; }
+        }
+
+        private DeletableContext GetTestContext()
+        {
+            var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PeterDoStuffDb;Integrated Security=True;Connect Timeout=10;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var options = new DbContextOptionsBuilder<DeletableContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+
+            return new DeletableContext(options);
+        }
+
+        [TestMethod]
+        public void _01_Migrator()
+        {
+            using var context = GetTestContext();
+            context.GetMigrator().GetCreateSql().WriteToConsole();
+        }
+    }
+}
---
+++ PeterDoStuff/Database/EF/DeletableDbSet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Database.EF
+{
+    public interface IDbSetContainer<TEntity> where TEntity : class
+    {
+        public DbSet<TEntity> Main { get; set; }
+    }
+
+    public class DeletableDbSet<TEntity> : IDbSetContainer<TEntity>
+        where TEntity : class
+    {
+        public DbSet<TEntity> Main { get; set; }
+    }
+}
