450c42625c708c757a5f2a55adebef07f1b1b6ca
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-04-13 22:39:04 +0800

    normalizer: Implement Add Dependency

---
+++ PeterDoStuff.Test/Tools/_01_SchemaNormalizer.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using PeterDoStuff.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Test.Tools
+{
+    [TestClass]
+    public class _01_SchemaNormalizer
+    {
+        [TestMethod]
+        public void _01_AddDependency()
+        {
+            var normalizer = SchemaNormalizer.New()
+                .AddDependency(["A", "B"], ["C"])
+                .AddDependency(["A"], ["D"])
+                .AddDependency(["A"], ["C", "D"]);
+
+            normalizer.GetAllAttributes().Should().HaveCount(4);
+
+            normalizer.Dependencies[0].LeftString.Should().Be("A, B");
+            normalizer.Dependencies[0].RightString.Should().Be("C");
+        }
+    }
+}
---
+++ PeterDoStuff/Tools/SchemaNormalizer.cs
@@ -0,0 +1,59 @@
+﻿using PeterDoStuff.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Tools
+{
+    public class SchemaNormalizer
+    {
+        public List<string> GetAllAttributes()
+        {
+            var allLefts = Dependencies.SelectMany(d => d.Left);
+            var allRights = Dependencies.SelectMany(d => d.Right);
+            return allLefts.Union(allRights).Distinct().ToList();
+        }
+
+        public static SchemaNormalizer New()
+        {
+            return new SchemaNormalizer();
+        }
+
+        public SchemaNormalizer AddDependency(string[] left, string[] right)
+        {
+            Dependencies.Add(new Dependency() { LeftString = left.Join(", "), RightString = right.Join(", ") });
+            return this;
+        }
+
+        public List<Dependency> Dependencies { get; private set; } = new List<Dependency>();
+
+        public class Dependency
+        {
+            public string LeftString
+            {
+                get => Left.Join(", ");
+                set => Left = value.Split(",").Select(x => x.Trim()).ToList();
+            }
+            public string RightString
+            {
+                get => Right.Join(", ");
+                set => Right = value.Split(", ").Select(x => x.Trim()).ToList();    
+            }
+            
+            public List<string> Left { get; private set; } = new List<string>();
+            public List<string> Right { get; private set; } = new List<string>();
+            
+            //public bool IsTrivial()
+            //{
+            //    foreach (var right in Right)
+            //    {
+            //        if (!Left.Contains(right))
+            //            return false;
+            //    }
+            //    return true;
+            //}
+        }
+    }
+}
