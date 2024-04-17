29fdc41b3d221834276963918a053a85851e9e34
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-04-17 13:33:32 +0800

    schema: Remove normalizer by now. Test only FuncDependency first.

---
+++ PeterDoStuff.Test/Tools/_01_Schema.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using PeterDoStuff.Tools.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Test.Tools
+{
+    [TestClass]
+    public class _01_Schema
+    {
+        [TestMethod]
+        public void _01_FuncDependency()
+        {
+            var fd1 = new FuncDependency("A, B", "A");
+            var fd2 = new FuncDependency("A, B", "C");
+            var fd3 = new FuncDependency("A, B", "A, C");
+
+            fd1.LeftString.Should().Be("A, B");
+            fd1.RightString.Should().Be("A");
+            fd1.Left.Should().HaveCount(2);
+            fd1.Right.Should().HaveCount(1);
+
+            fd1.IsTrivial().Should().BeTrue();
+            fd1.IsNonTrivial().Should().BeFalse();
+            fd1.IsCompletelyNonTrivial().Should().BeFalse();
+
+            fd2.IsTrivial().Should().BeFalse();
+            fd2.IsNonTrivial().Should().BeTrue();
+            fd2.IsCompletelyNonTrivial().Should().BeTrue();
+
+            fd3.IsTrivial().Should().BeFalse();
+            fd3.IsNonTrivial().Should().BeTrue();
+            fd3.IsCompletelyNonTrivial().Should().BeFalse();
+        }
+    }
+}
---
+++ PeterDoStuff/Tools/Schema/FuncDependency.cs
@@ -0,0 +1,55 @@
+﻿using PeterDoStuff.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterDoStuff.Tools.Schema
+{
+    public struct FuncDependency
+    {
+        public FuncDependency(string leftString, string rightString) 
+        {
+            LeftString = leftString;
+            RightString = rightString;
+        }
+
+        public string LeftString
+        {
+            get => Left.Join(", ");
+            set => Left = value.Split(",").Select(x => x.Trim()).ToList();
+        }
+        public string RightString
+        {
+            get => Right.Join(", ");
+            set => Right = value.Split(", ").Select(x => x.Trim()).ToList();
+        }
+
+        public List<string> Left { get; private set; } = new List<string>();
+        public List<string> Right { get; private set; } = new List<string>();
+
+        public bool IsTrivial()
+        {
+            foreach (var right in Right)
+            {
+                if (!Left.Contains(right))
+                    return false;
+            }
+            return true;
+        }
+
+        public bool IsNonTrivial()
+            => IsTrivial() == false;
+
+        public bool IsCompletelyNonTrivial()
+        {
+            if (IsTrivial() == true) return false;
+
+            foreach (var right in Right)
+                if (Left.Contains(right)) return false;
+
+            return true;
+        }
+    }
+}
