e5adea62630d0a7febf704982c936887de83cb18
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-11-11 04:09:26 +0800

    Remove DoNothing Animation.

--- PeterDoStuff/Tools/Graphics/DoNothing.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PeterDoStuff.Tools.Graphics
-{
-    public class DoNothing : Animation
-    {
-        public override Task Tick(Model model, TimeSpan? timeSpan = null) => Task.CompletedTask;
-    }
-}
