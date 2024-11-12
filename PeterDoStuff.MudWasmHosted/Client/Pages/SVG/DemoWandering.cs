577d94ce6277eeaab9800d0e7c1938b8f4b3225d
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-11-12 22:41:48 +0800

    Demo Wandering.

---
+++ PeterDoStuff.MudWasmHosted/Client/Pages/SVG/DemoWandering.cs
@@ -0,0 +1,24 @@
+﻿using PeterDoStuff.Tools.Graphics;
+
+namespace PeterDoStuff.MudWasmHosted.Client.Pages.SVG
+{
+    public class DemoWandering : Canvas
+    {
+        public DemoWandering(ModelBuilder? builder) : base(300, 300, builder)
+        {
+            Name = "Wandering";
+            
+            var circle = new Circle(10) { X = Width / 2, Y = Height / 2 };
+            circle.StrokeWidth = 4;
+            circle.AddAnimation(new Wander()
+            {
+                MinX = 30, MaxX = Width - 30,
+                MinY = 30, MaxY = Height - 30,
+                Velocity = 250,
+                SlowRange = 200
+            });
+
+            AddModel(circle);
+        }
+    }
+}
