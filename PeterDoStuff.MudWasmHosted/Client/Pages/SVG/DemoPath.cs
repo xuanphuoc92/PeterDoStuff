7404ba84b36cb06d453d5ddca22a1a74252d6a16
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-11-13 14:26:38 +0800

    Render PathModel.

---
+++ PeterDoStuff.MudWasmHosted/Client/Pages/SVG/DemoPath.cs
@@ -0,0 +1,22 @@
+﻿using PeterDoStuff.Tools.Graphics;
+
+namespace PeterDoStuff.MudWasmHosted.Client.Pages.SVG
+{
+    public class DemoPath : Canvas
+    {
+        public DemoPath(ModelStyler builder) : base(300, 300, builder)
+        {
+            Name = "Path";
+
+            var path = new PathModel(50, 150);
+            path.LineTo(100, 100);
+            path.LineTo(150, 150);
+            path.LineTo(200, 200);
+            path.LineTo(250, 150);
+
+            AddAndStyleModel(path);
+
+            path.FillOpacity = 0;
+        }
+    }
+}
