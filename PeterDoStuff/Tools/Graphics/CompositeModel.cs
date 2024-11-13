73457a8478bed76210500d2967a93f9ac2d259b9
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-11-13 18:17:14 +0800

    Put CompositeModel into its own class file.

---
+++ PeterDoStuff/Tools/Graphics/CompositeModel.cs
@@ -0,0 +1,19 @@
+﻿namespace PeterDoStuff.Tools.Graphics
+{
+    public class CompositeModel : Model
+    {
+        public List<Model> Children { get; } = [];
+
+        public CompositeModel(params Model[] children)
+        {
+            Children.AddRange(children);
+        }
+
+        public override async Task Resolve(DateTime? now = null)
+        {
+            await base.Resolve(now);
+            foreach (var child in Children)
+                await child.Resolve(now);
+        }
+    }
+}
--- PeterDoStuff/Tools/Graphics/Model.cs
+++ PeterDoStuff/Tools/Graphics/Model.cs
@@ -32,23 +32,6 @@
         public double ScaledStrokeWidth => StrokeWidth * Scale;
     }
 
-    public class CompositeModel : Model
-    {
-        public List<Model> Children { get; } = [];
-
-        public CompositeModel(params Model[] children)
-        {
-            Children.AddRange(children);
-        }
-
-        public override async Task Resolve(DateTime? now = null)
-        {
-            await base.Resolve(now);
-            foreach (var child in Children)
-                await child.Resolve(now);
-        }
-    }
-
     public static class ModelExtensions
     {
         public static TModel ClearAnimations<TModel>(this TModel model)
