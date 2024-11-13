6311397e5dee87b33331ec0c7428600957f58681
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-11-13 23:15:33 +0800

    Demo Rotating.

--- PeterDoStuff.MudWasmHosted/Client/Pages/SVG/DemoArrowPlane.cs
+++ PeterDoStuff.MudWasmHosted/Client/Pages/SVG/DemoArrowPlane.cs
@@ -2,18 +2,20 @@
 
 namespace PeterDoStuff.MudWasmHosted.Client.Pages.SVG
 {
-    public class DemoPlaneArrow : Canvas
+    public class DemoArrowPlane : Canvas
     {
-        public DemoPlaneArrow(ModelStyler builder) : base(300, 300, builder)
+        public DemoArrowPlane(ModelStyler builder) : base(300, 300, builder)
         {
-            Name = "Plane Arrow";
+            Name = "Arrow Plane";
 
             double size = 20;
 
             var polygon = new Polygon(0, size * -2, size, size, 0, 0, size * -1, size);
             polygon.X = Width / 2;
             polygon.Y = Height / 2;
-            polygon.Degree = 90;
+
+            var rotating = new Rotating();
+            polygon.AddAnimation(rotating);
 
             AddAndStyleModel(polygon);
         }
