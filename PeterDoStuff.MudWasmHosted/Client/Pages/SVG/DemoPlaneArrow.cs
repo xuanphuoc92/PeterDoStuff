4278e88123f7e66b50318343075db929c6358706
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-11-13 22:57:15 +0800

    Demo Polygon.

---
+++ PeterDoStuff.MudWasmHosted/Client/Pages/SVG/DemoPlaneArrow.cs
@@ -0,0 +1,20 @@
+﻿using PeterDoStuff.Tools.Graphics;
+
+namespace PeterDoStuff.MudWasmHosted.Client.Pages.SVG
+{
+    public class DemoPlaneArrow : Canvas
+    {
+        public DemoPlaneArrow(ModelStyler builder) : base(300, 300, builder)
+        {
+            Name = "Plane Arrow";
+
+            double size = 20;
+
+            var polygon = new Polygon(0, size * -2, size, size, 0, 0, size * -1, size);
+            polygon.X = Width / 2;
+            polygon.Y = Height / 2;
+
+            AddAndStyleModel(polygon);
+        }
+    }
+}
