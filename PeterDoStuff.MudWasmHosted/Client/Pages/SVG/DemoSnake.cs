2ee0493910ded63f69361f5e01f248a4ce7f157c
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-11-17 21:13:24 +0800

    Try drawing the spine line.

--- PeterDoStuff.MudWasmHosted/Client/Pages/SVG/DemoSnake.cs
+++ PeterDoStuff.MudWasmHosted/Client/Pages/SVG/DemoSnake.cs
@@ -11,8 +11,27 @@ namespace PeterDoStuff.MudWasmHosted.Client.Pages.SVG
             var chain = new Chain(24, 15, CreateJoint);
             Add(chain);
 
+            var spineLine = CreateLine(chain);
+            Add(spineLine);
+
             var head = chain.Joints[0];
             head.AddEffect(new Follow(Mouse) { Velocity = 1000, SlowRange = 100 });
+        }
+
+        private PathModel CreateLine(Chain chain)
+        {
+            var pathModel = new PathModel(0, 0);
+            pathModel.Style = Style.Clone();
+            pathModel.Style.StrokeWidth = 2;
+            pathModel.Style.FillOpacity = 0;
+            pathModel.Models[0].References.Add(chain.Joints[0]);
+
+            for (int i = 1; i < chain.Joints.Count; i++)
+            {
+                pathModel.LineTo(0, 0);
+                pathModel.Models.Last().References.Add(chain.Joints[i]);
+            }
+            return pathModel;
         }
 
         private CompositeModel CreateJoint(int index)
