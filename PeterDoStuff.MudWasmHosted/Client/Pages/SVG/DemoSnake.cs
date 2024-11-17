b8feadb27c294637371a5496ec80969dedef454a
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-11-17 12:04:03 +0800

    Create Snake demo.

---
+++ PeterDoStuff.MudWasmHosted/Client/Pages/SVG/DemoSnake.cs
@@ -0,0 +1,33 @@
+﻿using PeterDoStuff.Tools.Graphics;
+
+namespace PeterDoStuff.MudWasmHosted.Client.Pages.SVG
+{
+    public class DemoSnake : Canvas
+    {
+        public DemoSnake(Style? style = null) : base(300, 300, style)
+        {
+            Name = "Snake";
+
+            var chain = new Chain(24, 15, CreateJoint);
+            Add(chain);
+
+            var head = chain.Joints[0];
+            head.AddEffect(new Follow(Mouse) { Velocity = 1000, SlowRange = 100 });
+        }
+
+        private CompositeModel CreateJoint(int index)
+        {
+            var joint = new CompositeModel();
+
+            var jointShape = new Circle(64 / 4);
+            jointShape.Style = Style.Clone();
+            jointShape.Style.StrokeWidth = 2;
+            jointShape.Style.FillOpacity = 0;
+
+            joint.Models.Add(jointShape);
+
+            joint.AddEffect(j => jointShape.MoveTo(j));
+            return joint;
+        }
+    }
+}
