4428b956a9c2ba9b774a1804396ba1af6aece1c7
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-11-17 21:03:12 +0800

    Add MoveTo Effect.

---
+++ PeterDoStuff/Tools/Graphics/MoveTo.cs
@@ -0,0 +1,14 @@
+﻿
+namespace PeterDoStuff.Tools.Graphics
+{
+    public class MoveTo(Model anchor) : Effect
+    {
+        public Model Anchor = anchor;
+
+        protected override Task Tick(DateTime now)
+        {
+            Model.MoveTo(anchor);
+            return Task.CompletedTask;
+        }
+    }
+}
