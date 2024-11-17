f1107542be2e22cdc7cba0edd5a6171fe6b6d09f
xuanphuoc92 <xuanphuoc92@gmail.com>
2024-11-18 01:12:37 +0800

    Demo Distance Constraint.

--- PeterDoStuff.MudWasmHosted/Client/Pages/Svg/Gallery.cs
+++ PeterDoStuff.MudWasmHosted/Client/Pages/Svg/Gallery.cs
@@ -6,7 +6,30 @@ namespace PeterDoStuff.MudWasmHosted.Client.Pages.Svg
     public class Gallery(Style style)
     {
         public CanvasModel[] Canvases = [
-            new _01_BouncingBalls(style, new CircleModel(10))
+            new _01_BouncingBalls(style, new CircleModel(10)),
+            new _02_DistanceConstraint(style),
         ];
+    }
+
+    public class DistanceConstraint(Model anchor, double maxDistance) : Effect
+    {
+        public Model Anchor = anchor;
+        public double MaxDistance = maxDistance;
+        
+        public override void Tick()
+        {
+            Models.ForEach(m =>
+            {
+                var dx = Anchor.X - m.X;
+                var dy = Anchor.Y - m.Y;
+                var d = Math.Sqrt(dx * dx + dy * dy);
+
+                if (d > MaxDistance)
+                {
+                    m.X = Anchor.X - MaxDistance * dx / d;
+                    m.Y = Anchor.Y - MaxDistance * dy / d;
+                }
+            });
+        }
     }
 }
---
+++ PeterDoStuff.MudWasmHosted/Client/Pages/Svg/_02_DistanceConstraint.cs
@@ -0,0 +1,20 @@
+﻿using PeterDoStuff.Graphics;
+using PeterDoStuff.Graphics.Models;
+
+namespace PeterDoStuff.MudWasmHosted.Client.Pages.Svg
+{
+    public class _02_DistanceConstraint : CanvasModel
+    {
+        public _02_DistanceConstraint(Style? style = null) : base(300, 300, style, new CircleModel(50))
+        {
+            Name = "Distance Constraint";
+
+            Mouse.Style.StrokeOpacity *= 0.5;
+            Mouse.Style.FillOpacity *= 0.1;
+
+            var circle = new CircleModel(10);
+            circle.Apply(new DistanceConstraint(Mouse, 50));
+            AddAndStyle(circle);
+        }
+    }
+}
