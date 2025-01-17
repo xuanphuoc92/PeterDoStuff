488c5b7a32a88d053d290897d32600a9d7791b1a
xuanphuoc92 <xuanphuoc92@gmail.com>
2025-01-17 10:03:03 +0800

    Doc Template: Create the demo page.

--- PeterDoStuff.MudWasmHosted/Client/PlaygroundItem.cs
+++ PeterDoStuff.MudWasmHosted/Client/PlaygroundItem.cs
@@ -21,6 +21,7 @@ namespace PeterDoStuff.MudWasmHosted.Client
 
         public static readonly List<PlaygroundItem> Items = new List<PlaygroundItem>()
         {
+            new(PlaygroundGroup.Demos, "docTemplate", @Icons.Custom.FileFormats.FileImage, "Template", "This page demonstrates template rendering."),
             new(PlaygroundGroup.Tools, "schemaTools", Icons.Material.Filled.Schema, "Schema Tools", "This page demonstrates the tools for schema designing."),
             new(PlaygroundGroup.Demos, "svg", @Icons.Custom.Uncategorized.Fish, "SVG", "This page demonstrates SVG rendering and animations."),
             new(PlaygroundGroup.Demos, "whatsapp", Icons.Custom.Brands.WhatsApp, "Chat on WhatsApp", "This page demonstrates WhatsApp How to use click to chat."),
