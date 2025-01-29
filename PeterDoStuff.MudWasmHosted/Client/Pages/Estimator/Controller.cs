c7d49a8ba8930727efd2b2f60836560a6c57d94d
xuanphuoc92 <xuanphuoc92@gmail.com>
2025-01-29 11:53:04 +0700

    Estimator: Create the basic Project model.

---
+++ PeterDoStuff.MudWasmHosted/Client/Pages/Estimator/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using static PeterDoStuff.MudWasmHosted.Client.Pages.Estimator.Controller;
+
+namespace PeterDoStuff.MudWasmHosted.Client.Pages.Estimator
+{
+    internal static class EstimatorExtensions
+    {
+    }
+
+    public class Controller
+    {
+        public class Project
+        {
+            public string Name { get; set; } = "My Project";
+        }
+    }
+}
