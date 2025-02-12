f1339d7b3f726d55a481d26f0b833f52f464bfbe
xuanphuoc92 <xuanphuoc92@gmail.com>
2025-02-12 14:12:13 +0800

    MyFinance: Refactor the DownloadJson extension.

---
+++ PeterDoStuff.MudWasmHosted/Client/Extensions/JSRuntimeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+using PeterDoStuff.Extensions;
+using System.Text;
+
+namespace PeterDoStuff.MudWasmHosted.Client.Extensions
+{
+    public static class JSRuntimeExtensions
+    {
+        public static async Task DownloadJson(this IJSRuntime JS, object obj, string fileName, string defaultFileName = "Default")
+        {
+            if (fileName.IsNullOrEmpty())
+                fileName = defaultFileName;
+            
+            if (fileName.ToLowerInvariant().EndsWith(".json") == false)
+                fileName += ".json";
+
+            using MemoryStream fileStream = new MemoryStream(
+                Encoding.UTF8.GetBytes(obj.ToJson(beautify: true)));
+            using var streamRef = new DotNetStreamReference(stream: fileStream);
+            await JS.InvokeVoidAsync("downloadFileFromStream", fileName.Trim(), streamRef);
+        }
+    }
+}
