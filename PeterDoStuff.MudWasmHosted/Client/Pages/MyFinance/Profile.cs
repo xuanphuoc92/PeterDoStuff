c95ded92ad35c0706633ef3e4f86c686dc9ac751
xuanphuoc92 <xuanphuoc92@gmail.com>
2025-02-12 13:50:27 +0800

    MyFinance: Create example Blocks and Controls.

---
+++ PeterDoStuff.MudWasmHosted/Client/Pages/MyFinance/Profile.cs
@@ -0,0 +1,38 @@
+﻿namespace PeterDoStuff.MudWasmHosted.Client.Pages.MyFinance
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public List<Block> Blocks { get; set; } = [];
+    }
+
+    public abstract class Block
+    {
+        public abstract string Name { get; set; }
+        public abstract decimal Value { get; set; }
+        public abstract string Currency { get; set; }
+    }
+
+    public class UsdBlock : Block
+    {
+        public override string Name { get; set; }
+        public override decimal Value { get; set; }
+        public override string Currency { get; set; } = "USD";
+    }
+
+    public class VndBlock : Block
+    {
+        public override string Name { get; set; }
+        public override decimal Value { get; set; }
+        public override string Currency { get; set; } = "VND";
+    }
+
+    public class Controller
+    {
+        public static Profile SampleProfile()
+            => new()
+            {
+                Name = "John Doe",
+            };
+    }
+}
