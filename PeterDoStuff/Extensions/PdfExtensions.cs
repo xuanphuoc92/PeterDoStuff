f550f99658c869c1cfe2cf481673986e0a419d33
xuanphuoc92 <xuanphuoc92@gmail.com>
2025-02-21 16:38:41 +0800

    PdfExtensions: Fix the failure in later Chrome/Edge, by using the portable version.

--- PeterDoStuff/Extensions/PdfExtensions.cs
+++ PeterDoStuff/Extensions/PdfExtensions.cs
@@ -13,7 +13,7 @@ namespace PeterDoStuff.Extensions
         public static void ConvertUriToPdf(this string uri, string pdfFilePath)
         {
             var pageSettings = new PageSettings();
-            using var converter = new Converter("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe");
+            using var converter = new Converter("D:\\Chrome120\\App\\Chrome-bin\\chrome.exe");
             converter.UseOldHeadlessMode = true;
             converter.ConvertToPdf(new ConvertUri(uri), pdfFilePath, pageSettings);
         }
@@ -21,7 +21,7 @@ namespace PeterDoStuff.Extensions
         public static void ConvertFileToPdf(this string htmlFilePath, string pdfFilePath)
         {
             var pageSettings = new PageSettings();
-            using var converter = new Converter("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe");
+            using var converter = new Converter("D:\\Chrome120\\App\\Chrome-bin\\chrome.exe");
             converter.UseOldHeadlessMode = true;
             string html = File.ReadAllText(htmlFilePath);
             converter.ConvertToPdf(html, pdfFilePath, pageSettings);
